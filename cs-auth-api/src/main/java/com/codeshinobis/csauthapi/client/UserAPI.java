984119b26ed129d7cf4753d617725aaee4506af1
praveenapk26 <rahanepraveen@gmail.com>
2023-08-06 11:06:00 +0000

    Added encrytion and decryption in user api

--- cs-auth-api/src/main/java/com/codeshinobis/csauthapi/client/UserAPI.java
+++ cs-auth-api/src/main/java/com/codeshinobis/csauthapi/client/UserAPI.java
@@ -1,9 +1,18 @@
 package com.codeshinobis.csauthapi.client;
 
-import com.codeshinobis.csauthapi.client.model.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.codeshinobis.csauthapi.client.model.User;
+import com.codeshinobis.csauthapi.model.ResponseDto;
+
+@FeignClient(value = "user-profile-api", url="lb://cs-user-profile-api")
 public interface UserAPI {
 
-    User getUser(String userId);
+    @GetMapping("/api/cs/user-profile/user/{userId}")
+    
+    ResponseEntity<ResponseDto<User>> getUser(@PathVariable String userId);
     
 }
